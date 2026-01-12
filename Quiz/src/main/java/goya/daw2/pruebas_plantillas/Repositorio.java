a33da3ac4112cc885e665b4ece455ece08ac50bb
Freddy De Andrade Bernabeu <freddydeandrade54@gmail.com>
2026-01-12 09:15:25 +0100

    Delete Quiz directory

--- Quiz/src/main/java/goya/daw2/pruebas_plantillas/Repositorio.java
+++
@@ -1,9 +0,0 @@
-package goya.daw2.pruebas_plantillas;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface Repositorio extends CrudRepository<QuizBBDD,String> {
-	QuizBBDD findByNombre(String nombre);
-}
